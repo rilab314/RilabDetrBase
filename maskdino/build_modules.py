e93cc253b804ede83ec6422526b61e58d4c977c8
goodgodgd <goodgodgd@yonsei.ac.kr>
2024-08-30 17:29:46 +0900

    predict maskdino // print structure of prediction output

--- maskdino/build_modules.py
+++ maskdino/build_modules.py
@@ -1,19 +1,4 @@
-import argparse
-import os
-import sys
-import tempfile
-import time
-import warnings
-import cv2
-import numpy as np
-import tqdm
-
-from detectron2.config import get_cfg
-from detectron2.data.detection_utils import read_image
-from detectron2.projects.deeplab import add_deeplab_config
-
-import maskdino
-from maskdino.config import add_maskdino_config
+from maskdino.utils.setup_cfg import setup_cfg
 from maskdino.backbone.swin import D2SwinTransformer
 from maskdino.pixel_decoder.maskdino_encoder import MaskDINOEncoder
 from maskdino.transformer_decoder.maskdino_decoder import MaskDINODecoder
@@ -23,8 +8,7 @@ from maskdino.maskdino_model import MaskDINO
 
 
 def create_modules():
-    args = get_parser().parse_args()
-    cfg = setup_cfg(args)
+    cfg = setup_cfg()
     print('\n========== configs ==========\n', cfg)
     backbone = D2SwinTransformer(cfg)
     print('\n========== backbone ==========\n', backbone)
@@ -38,54 +22,6 @@ def create_modules():
     print('\n========== criterion ==========\n', criterion)
     model = MaskDINO(cfg)
     print('\n========== model ==========\n', model)
-
-
-def setup_cfg(args):
-    # load config from file and command-line arguments
-    cfg = get_cfg()
-    add_deeplab_config(cfg)
-    add_maskdino_config(cfg)
-    cfg.merge_from_file(args.config_file)
-    cfg.merge_from_list(args.opts)
-    cfg.freeze()
-    return cfg
-
-
-def get_parser():
-    parser = argparse.ArgumentParser(description="maskdino demo for builtin configs")
-    parser.add_argument(
-        "--config-file",
-        default="/home/dolphin/choi_ws/SatLaneDet_2024/SatelliteLaneDet2024/maskdino/configs/maskdino_R50_bs16_50ep_4s_dowsample1_1024.yaml",
-        metavar="FILE",
-        help="path to config file",
-    )
-    parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
-    parser.add_argument("--video-input", help="Path to video file.")
-    parser.add_argument(
-        "--input",
-        nargs="+",
-        help="A list of space separated input images; "
-             "or a single glob pattern such as 'directory/*.jpg'",
-    )
-    parser.add_argument(
-        "--output",
-        help="A file or directory to save output visualizations. "
-             "If not given, will show output in an OpenCV window.",
-    )
-
-    parser.add_argument(
-        "--confidence-threshold",
-        type=float,
-        default=0.5,
-        help="Minimum score for instance predictions to be shown",
-    )
-    parser.add_argument(
-        "--opts",
-        help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=[],
-        nargs=argparse.REMAINDER,
-    )
-    return parser
 
 
 def create_matcher(cfg):
